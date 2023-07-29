20933237a0c62af74b4e39a7f600d15d9221cf1a
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-29 21:00:38 +0200

    Update Store.jsx

--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,12 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit';
-import contactsReducer from './components/ContactList/contactsSlice';
-import filterReducer from './components/Filter/filterSlice';
 
-const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-    filter: filterReducer,
+import { configureStore, createSlice } from '@reduxjs/toolkit';
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: [],
+  reducers: {
+    addContact: (state, action) => {
+      state.push(action.payload);
+    },
+    deleteContact: (state, action) => {
+      return state.filter((contact) => contact.id !== action.payload);
+    },
   },
 });
 
-export default store;
+export const { addContact, deleteContact } = contactsSlice.actions;
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: '',
+  reducers: {
+    setFilter: (state, action) => {
+      return action.payload;
+    },
+  },
+});
+
+export const { setFilter } = filterSlice.actions;
+
+const reducer = {
+  contacts: contactsSlice.reducer,
+  filter: filterSlice.reducer,
+};
+
+export const store = configureStore({
+  reducer,
+});
