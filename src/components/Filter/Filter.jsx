280d683a4f9d10ea56d100a7015cf7ffb491dde3
Jakub Piksa <kuba.piksa@wp.pl>
2023-07-30 12:51:22 +0200

    FILTER MI NIE DIZAŁA GRRR

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { setFilter } from '../../store/store'; // Poprawiona ścieżka importu
+import { setFilter } from '../../store/store'; 
 import css from './filter.module.css';
 
 const Filter = () => {
